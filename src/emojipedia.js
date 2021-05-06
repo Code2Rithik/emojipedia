cc0c602ed43a1deeaa9360b7a011783164c6742d
Rithik Samanthula <talk2rithiks@gmail.com>
2021-05-06 07:16:09 -0400

    Upload files

---
+++ src/emojipedia.js
@@ -0,0 +1,95 @@
+const emojipedia = [
+  {
+    id: 1,
+    emoji: "💪",
+    name: "Tense Biceps",
+    meaning:
+      "“You can do that!” or “I feel strong!” Arm with tense biceps. Also used in connection with doing sports, e.g. at the gym."
+  },
+  {
+    id: 2,
+    emoji: "🙏",
+    name: "Person With Folded Hands",
+    meaning:
+      "Two hands pressed together. Is currently very introverted, saying a prayer, or hoping for enlightenment. Is also used as a “high five” or to say thank you."
+  },
+  {
+    id: 3,
+    emoji: "🤣",
+    name: "Rolling On The Floor, Laughing",
+    meaning:
+      "This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for „rolling on the floor, laughing“."
+  },
+  {
+    id: 4,
+    emoji: "😂",
+    name: "Face With Tears of Joy",
+    meaning:
+      "I am laughing to hard that I can hardly keep myself on my chair! Something is so incredibly funny that you cry laughing. Gets the giggles and can hardly contain himself. This emoji is the most popular and was named Word of the Year 2015."
+  },
+  {
+    id: 5,
+    emoji: "😅",
+    name: "Smiling Face With Open Mouth & Cold Sweat",
+    meaning:
+      "TSweaty smile. Relief that a tight situation went well or was coped with positively, e.g. an important appointment. A difficult event is imminent and you are already nervous."
+  },
+  {
+    id: 6,
+    emoji: "🥲",
+    name: "Smiling face with tear",
+    meaning:
+      "Smile, even in difficult times. Instead of being really happy, you smile despite the pain. The real feelings are hidden behind a smile. Someone is going through difficult times or has a lot of problems."
+  },
+  {
+    id: 7,
+    emoji: "😊",
+    name: "Smiling Face With Smiling Eyes",
+    meaning:
+      "Is grinning shyly and embarrassed. The red cheeks are an expression of joy. Represents happiness, contentment, peace of mind and gratitude. Text-based variant: ^^."
+  },
+  {
+    id: 8,
+    emoji: "😇",
+    name: "Smiling Face With Halo",
+    meaning:
+      "Would like to express its (possibly pretended) innocence. Someone does good deeds, is very sweet, innocent and behaves in an exemplary way. The angelic face can also be used humorously for rather not so good deeds or behaviors."
+  },
+  {
+    id: 9,
+    emoji: "😉",
+    name: "Winking Face",
+    meaning:
+      "Smiley blinks mischievously with one eye. Expression of humor, has been pulling a practical joke, is flirting with you or has an ulterior motive. The wink weakens the message: do not take it seriously, regard it as humorous."
+  },
+  {
+    id: 10,
+    emoji: "😌",
+    name: "Relieved Face",
+    meaning:
+      "Everything went well! Relieved smiley face. Is happy that something unpleasant is over without having caused any harm. Relaxed, thankful and free of worries."
+  },
+  {
+    id: 11,
+    emoji: "😋",
+    name: "Face Savouring Delicious Food",
+    meaning:
+      "Bon appetit! Smiley is licking the corner of its mouth with its tongue. Is hungry, just cooked something very delicious or eaten a whole dish. Often interpreted as a grimace, with tongue outstretched, and used after a funny message or when fooling around."
+  },
+  {
+    id: 12,
+    emoji: "😝",
+    name: "Face With Stuck-Out Tongue & Tightly Closed Eyes",
+    meaning:
+      "Making fun of someone, annoying or jokingly offending someone. In provocative news, crude or black humor as well as with glee."
+  },
+  {
+    id: 13,
+    emoji: "🤪",
+    name: "Crazy Face",
+    meaning:
+      "Something is insanely funny. The mood is exuberant - you are totally silly and crazy. Totally freaking out with enthusiasm or joy. Maybe someone has even told an indecent joke."
+  }
+];
+
+export default emojipedia;
