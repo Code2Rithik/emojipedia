c019d6e0a3568ff3b59ccba123c624d77db8e93c
Rithik Samanthula <talk2rithiks@gmail.com>
2021-05-06 07:20:22 -0400

    Updates

---
+++ precache-manifest.9096908bed1059532cf381be659416ba.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5fbabca4f7b5455f4afd1e160676ef93",
+    "url": "/emojipedia/index.html"
+  },
+  {
+    "revision": "aee04822626c08a5b3a6",
+    "url": "/emojipedia/static/js/2.564df045.chunk.js"
+  },
+  {
+    "revision": "f999b2b4c996166ecfea",
+    "url": "/emojipedia/static/js/main.ddcc45e7.chunk.js"
+  },
+  {
+    "revision": "b23b46efe5a8f40d9a29",
+    "url": "/emojipedia/static/js/runtime-main.76d70354.js"
+  }
+]);
